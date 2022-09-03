6ab2afbcf2c275464c4ae0a2fb31921383e95db5
abdulhamid331404 <freelancerhamid1@gmail.com>
2022-09-04 00:58:48 +0600

    add found section

--- js/main.js
+++ js/main.js
@@ -6,35 +6,57 @@ const loadCategory = () =>{
 }
 
 const displayCategoryData = (categorys) =>{
+
   const categoryContainer = document.getElementById('category-container');
+
   categoryContainer.classList.add('bg-light')
   categoryContainer.classList.add('navbar')
   categorys.forEach(category =>{
-   
+  //  console.log(category);
     const categoryDiv = document.createElement('div');
     categoryDiv.classList.add('d-flex')
     categoryDiv.innerHTML = `
-    <a onclick="loadNewsCategory(console.log('hello'))" class="navbar-brand" href="#">${category.category_name}</a>
+   <a onclick="loadNewsCategory('${category.category_id}')" class="navbar-brand" href="#">${category.category_name}</a>
     `
+    // onclick="${loadNewList(categories[0].category_id)}"
     categoryContainer.appendChild(categoryDiv);
   })
+
 }
 
-const loadNewsCategory = () =>{
-    const url1 = `https://openapi.programming-hero.com/api/news/category/08 `
-    fetch(url1)
+const loadNewsCategory = (category_id) =>{
+    // console.log(category_id);
+    const url = `https://openapi.programming-hero.com/api/news/category/${category_id}`
+    // console.log(url);
+    fetch(url)
     .then(res =>res.json())
     .then(data => displayNewsCategory(data.data))
 
+    toggleSpinner(true);
 }
 
 const displayNewsCategory = (newsCategorys)  =>{
-    const newsCategorysContainer = document.getElementById('news-categorys')
+
+  // result founed
+  const resultFound = document.getElementById('result-found');
+   
+  if (newsCategorys.length === 0) {
+        resultFound.innerHTML = `<span class="text-danger bg-light rounded navbar bg-light mt-3 ">Result not found</sapn>`
+    }
+    else {
+        resultFound.innerHTML = `<span class="text-success navbar bg-light mt-3 ">Total result found ${newsCategorys.length}</span>`
+    }
+
+
+  // console.log(newsCategorys);
+    const newsCategorysContainer = document.getElementById('news-categorys');
+
+    newsCategorysContainer.innerHTML = '';
+
     newsCategorys.forEach(newsCategory =>{
-        console.log(newsCategory);
+        // console.log(newsCategory);
        const newsList = document.createElement('div');
-        newsList.classList.add('card')
-        newsList.classList.add('m-2')
+        newsList.classList.add('card','m-3')
        const categoryDiv = document.createElement('div');
        categoryDiv.classList.add('row');
        categoryDiv.innerHTML = `
@@ -45,8 +67,28 @@ const displayNewsCategory = (newsCategorys)  =>{
                   <div class="col-lg-8">
                     <div class="card-body">
                       <h5 class="card-title">${newsCategory.title}</h5>
-                      <p class="card-text">${newsCategory.details}</p>
-                      <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
+                      <p class="card-text">${newsCategory.details.slice(1, 500)}...</p>
+                      <div class="row">
+                     <div class="col col-lg-3 ">
+                    <div class="d-flex">
+                    <img style="height:38px; widht: 38px;" src="${newsCategory.author.img}" alt="" />
+                    <h6>${newsCategory.author.name ? newsCategory.author.name : 'No Author' }</h6>
+                    
+                    </div>
+                     <p>${newsCategory.author.published_date ? newsCategory.author.published_date : 'Unabilable' }</p>
+                     </div>
+                     <div class="col col-lg-3">
+                      <p> <i class="fa-regular fa-eye"></i> ${newsCategory.total_view  ? newsCategory.total_view : 'No View'}</p>
+                     </div>
+                     <div class="col col-lg-3">
+                     <p>
+                     <i class="fa-solid fa-star-half-stroke"></i>
+                     <i class="fa-regular fa-star"></i><i class="fa-regular fa-star"></i><i class="fa-regular fa-star"></i><i class="fa-regular fa-star"></i></p>
+                     </div>
+                     <div class="col col-lg-3 fs-4 text-center">
+                     <p><i class="fa-solid fa-arrow-right"></i></p>
+                     </div>
+                      </div>
                     </div>
                   </div>
        
@@ -55,10 +97,31 @@ const displayNewsCategory = (newsCategorys)  =>{
        newsCategorysContainer.appendChild(newsList);
 
     })
+    toggleSpinner(false)
+}
+
+// loadNewsCategory()
+
+
+const toggleSpinner = isLoading  => {
+  const loaderSection = document.getElementById('loader');
+  if(isLoading){
+      loaderSection.classList.remove('d-none');
+  }
+  else{
+      loaderSection.classList.add('d-none')
+  }
+  
 }
 
 
-
-
+const displayNews = () =>{
+  const url = `https://openapi.programming-hero.com/api/news/0282e0e58a5c404fbd15261f11c2ab6a`;
+  fetch(url)
+    .then(res =>res.json())
+    .then(data => console.log(data.data[0]))
+  
+}
+displayNews()
 
 loadCategory();
